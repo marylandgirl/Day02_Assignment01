67ae894c9304f3b514fbcc6a7d68cb38b1b41565
Kim Levin <kbussie1@montgomerycollege.edu>
2020-04-14 10:43:57 -0400

    First commit

---
+++ src/MySecondJava.java
@@ -0,0 +1,21 @@
+public class MySecondJava {
+    public static void main(String[] args){
+        boolean flag = true;
+        byte b = 4;
+        char a = 'A';
+        short index = 3;
+        int i = 89;
+        float f = 4.73f;
+        long longVar = 1234567890;
+        double doubleVar = 123456789.123456789;
+
+        System.out.println("flag: " + flag);
+        System.out.println("byte: " + b);
+        System.out.println("char: " + a);
+        System.out.println("short: " + index);
+        System.out.println("i: " + i);
+        System.out.println("f: " + f);
+        System.out.println("longVar: " + longVar);
+        System.out.println("doubleVar: " + doubleVar);
+    }
+}
